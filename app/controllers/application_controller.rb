f3c5245aeee3caec4edc9fef64c454ec9eaacabc
VitaliiKostyrka <kosvit1408@gmail.com>
2019-06-28 18:53:58 +0300

    edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :email, :password, :password_confirmation)
-    end
-    devise_parameter_sanitizer.permit(:account_update) do |u|
-      u.permit(:name, :email)
-    end
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
   def index
     @instance_var = 2
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to root_path, notice: "Your profile updated"
+    else
+      @error = @user.errors.full_messages
+    end
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  protected
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
