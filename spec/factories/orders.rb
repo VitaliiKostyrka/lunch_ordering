0c521d702f470e667f9a5a1505f90195717c113d
Vitalii Kostyrka <vkosturka@solanolabs.com>
2019-07-03 12:14:23 +0300

    add test to admin_page

--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
-  factory :Order do
-    association :menu_item, factory: menu_item
-    association :user, factory: user
+  factory :order do
+    association :menu_item, factory: :menu_item
+    association :user, factory: :user
   end
 end
