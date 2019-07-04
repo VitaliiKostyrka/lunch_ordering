6ff48fd461d13de7c15dea81cef03f0332a22c64
Vitalii Kostyrka <vkosturka@solanolabs.com>
2019-07-04 10:23:48 +0300

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  before_save :check_to_make_admin
   has_many :orders
+  before_save :check_to_make_admin
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :name, :email, :encrypted_password, presence: true
